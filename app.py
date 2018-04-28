d711b9ace78829e1d25a9f22fa6bbe6392d23f0d
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-04-27 20:24:30 -0700

    added persistent storage -- sqlite databaset -- very simple as you do not have to run databaset

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
@@ -14,7 +16,6 @@ from time import sleep
 from multiprocessing import Manager
 
 import random
-
 
 tasks = []
 losses_arrays = []
@@ -36,6 +37,28 @@ def slow_function(process_list):
 
 
 server = Flask(__name__)
+
+
+# Creating database connection and initializing the database in case
+# it's the first time the app is run
+
+database_file_location = "~/.dash-deep/experiments.db"
+
+database_file_location = os.path.expanduser( database_file_location )
+
+database_path = os.path.dirname(database_file_location)
+
+if not os.path.exists(database_path):
+    
+    os.makedirs(database_path)
+
+
+server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_file_location
+
+# Database object 
+db = SQLAlchemy(server)
+
+# Initializing the Dash application
 
 app = dash.Dash(__name__, server=server)
 
