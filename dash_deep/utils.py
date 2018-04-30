5f21ce9e51582d9f7c6abd5329cdf883599a6934
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-04-29 20:55:49 -0700

    changed a name of the function to be more explicit

--- dash_deep/utils.py
+++ dash_deep/utils.py
@@ -1,28 +1,27 @@
 
 
-def get_scripts_url_endpoints_names(script_db_models):
-    """Generates url endpoints names for each sqlalchemy model class representing a script.
+def get_script_files_basenames(script_db_models):
+    """Generates basenames (without .py ext though) for each sqlalchemy model class representing a script.
     
     Extracts the script name of a file in dash_deep/scripts/ folder in which the sqlalchemy
-    model class was defined. These names are later on used on the scripts/{endpoint} front page.
+    model class was defined. These names are later on used on the scripts/ front page.
     
     Parameters
     ----------
     script_db_models : list of sqlalchemy classes
-        List containing sqlalchemy classes which represent each experiment
+        List containing sqlalchemy classes which represent each script.
     
     Returns
     -------
-    scripts_url_endpoints_names : list of strings
-        List of strings where each string represents the display name of an experiment.
-        These strings are using to create url endpoints.
+    script_display_names : list of strings
+        List of strings where each string represents basename of a script file.
     """
     
     def get_parent_module_name(class_object):
         
         return class_object.__module__.split('.')[-1]
 
-    scripts_url_endpoints_names = map(lambda script_db_model:get_parent_module_name(script_db_model),
+    script_files_basenames = map(lambda script_db_model:get_parent_module_name(script_db_model),
                                script_db_models)
     
-    return scripts_url_endpoints_names
+    return script_files_basenames
