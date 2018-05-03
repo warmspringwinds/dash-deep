f5ab9b0618afac7accb2828142fc89f8ac399a97
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-05-02 19:35:06 -0700

    pushing temporary experimental updates

--- dash_deep/scripts/semantic_segmentation_train_script.py
+++ dash_deep/scripts/semantic_segmentation_train_script.py
@@ -1,12 +1,34 @@
 from dash_deep.app import db
+from time import sleep
 
 
 class PascalSegmentation(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
-    batch_size = db.Column(db.String(80), unique=True, nullable=False)
-    learning_rate = db.Column(db.String(120), unique=True, nullable=False)
-    output_stride = db.Column(db.String(120), unique=True, nullable=False)
-
+    batch_size = db.Column(db.String(80), nullable=False)
+    learning_rate = db.Column(db.String(120), nullable=False)
+    output_stride = db.Column(db.String(120), nullable=False)
+    
+    actions = {}
+    
     def __repr__(self):
         return '<Pascal Image Segmentation experiment>'
+
+
+def run(populated_object):
+    
+    db.engine.dispose()
+    
+    # Simulate training
+    sleep(7)
+    
+    populated_object.batch_size = '555'
+    populated_object.learning_rate = '100'
+    populated_object.output_stride = '777'
+    
+    db.session.add(populated_object)
+    db.session.commit()
+    
+    return 'success'
+
+PascalSegmentation.actions['main'] = run
