795f2c2b98aa9b5da389266f1d118585c018a6c9
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-08-02 22:57:53 -0400

    added simple dataset model

--- dash_deep/models.py
+++ dash_deep/models.py
@@ -2,6 +2,35 @@ from dash_deep.app import db
 from dash_deep.mixins import BasicExperimentMixin
 
 
+from dash_deep.scripts.endovis_binary_segmentation_train import simple_train_dataset, simple_val_dataset
+
+class SimpleBinary(BasicExperimentMixin, db.Model):
+    
+    title = 'Noname Binary Segmentation'
+    output_stride = db.Column(db.Integer, nullable=False)
+    
+    actions = {'main': lambda x: x,
+               'inference': lambda x: x}
+    
+    datasets = {'train': simple_train_dataset,
+                'valset': simple_val_dataset}
+    
+    
+    def __init__(self, *args, **kwargs):
+        
+        super(SimpleBinary, self).__init__(*args, **kwargs)
+        self.output_stride = 8
+        
+        
+    def __repr__(self):
+        
+        return ('<Noname Binary Segmentation experiment id={}, batch_size={}, learning_rate={}>, output_stride={}'.format(
+                self.id,
+                self.batch_size,
+                self.learning_rate,
+                self.output_stride))
+
+
 from dash_deep.scripts.endovis_binary_segmentation_train import run as endovis_binary_segmentation_train_run
 from dash_deep.scripts.endovis_binary_segmentation_train import inference as endovis_binary_segmentation_inference
 from dash_deep.scripts.endovis_binary_segmentation_train import valset, trainset
